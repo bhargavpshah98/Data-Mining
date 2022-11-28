24173810c389a5f30e1ab3eb5e7d47f9005b0467
Varun Alla <varun.alla@gmail.com>
2022-11-28 11:44:24 -0800

    baseline project

---
+++ Main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Data mining",
+    page_icon="🗺️",
+)
+st.write("# DataMining Project!")
+
+st.markdown(
+    """
+    Data Mining Project
+"""
+)
